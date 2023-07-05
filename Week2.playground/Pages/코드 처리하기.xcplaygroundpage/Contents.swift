3dcd7573646d6bb44feec2349cbb8ceb3d9fab74
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-05 15:58:58 +0900

    코드 처리하기 : 맨 처음 풀이

--- Week2.playground/Pages/코드 처리하기.xcplaygroundpage/Contents.swift
+++ Week2.playground/Pages/코드 처리하기.xcplaygroundpage/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ code:String) -> String {
+    var ret: String = ""
+    var mode: Int = 0
+    
+    for idx in 0..<code.count {
+        if code[code.index(code.startIndex, offsetBy: idx)] == "1" {
+            mode = mode == 0 ? 1 : 0
+        } else {
+            if idx % 2 == mode {
+                ret.append(code[code.index(code.startIndex, offsetBy: idx)])
+            }
+        }
+    }
+    
+    return ret.isEmpty ? "EMPTY" : ret
+}
+
+solution("abc1abc1abc")
