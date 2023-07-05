6c7c4c9be8970ce179b63bed9b09284c73dd5975
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-05 14:03:56 +0900

    연속된 수의 합 : 점수는 맞았지만 접근방식이 잘못된 풀이

--- Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
+++ Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
@@ -3,7 +3,8 @@
 import Foundation
 
 func solution(_ num:Int, _ total:Int) -> [Int] {
-    var i: Int = total, start: Int = 0, result: Int
+    // 배열의 가장 큰 수가 total보다 클 수 있음!! -> 이 풀이 완전 틀림
+    var i: Int = total + num, start: Int = 0, result: Int
     while true {
         result = 0
         for j in 0..<num {
@@ -18,6 +19,6 @@ func solution(_ num:Int, _ total:Int) -> [Int] {
     return Array((start - num + 1) ... start)
 }
 
-solution(5, 5)
+solution(4, 2)
 
 //: [Next](@next)
