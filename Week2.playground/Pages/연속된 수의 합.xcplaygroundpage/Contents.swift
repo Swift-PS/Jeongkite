b8f535fba63ff0002977962d45f856f0010bf461
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-05 11:57:33 +0900

    연속된 수의 합 맨 처음 생각한 방법

--- Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
+++ Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
@@ -2,4 +2,21 @@
 
 import Foundation
 
+func solution(_ num:Int, _ total:Int) -> [Int] {
+    var start: Int = 0, result: Int
+    for i in 1...100 {
+        result = 0
+        for j in 0..<num {
+            result += i + j
+        }
+        if result == total {
+            start = i
+            break
+        }
+    }
+    return Array(start ..< (start+num))
+}
+
+solution(3, 12)
+
 //: [Next](@next)
