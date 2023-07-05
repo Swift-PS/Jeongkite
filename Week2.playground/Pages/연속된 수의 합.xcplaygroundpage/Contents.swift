823e68b651866574fda611b2751160d0005db051
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-05 15:23:04 +0900

    연속된 수의 합 : 삼항연산자 사용

--- Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
+++ Week2.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
@@ -5,11 +5,7 @@ import Foundation
 func solution(_ num:Int, _ total:Int) -> [Int] {
     let mid: Int = total / num
     let count: Int = num / 2
-    if num % 2 == 0 {
-        return Array((mid-count+1)...(mid+count))
-    } else {
-        return Array((mid-count)...(mid+count))
-    }
+    return Array((num % 2 == 0 ? (mid-count+1) : (mid-count))...(mid+count))
 }
 
 solution(4, 2)
