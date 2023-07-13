4791198d8398966d8a9efcd69d88e2450c443c06
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-13 16:58:00 +0900

    세 선분간 중복된 값 파악하지 않음

--- Week3.playground/Pages/2_PRO_겹치는 선분의 길이.xcplaygroundpage/Contents.swift
+++ Week3.playground/Pages/2_PRO_겹치는 선분의 길이.xcplaygroundpage/Contents.swift
@@ -2,6 +2,26 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+import Foundation
+
+func solution(_ lines:[[Int]]) -> Int {
+    let compare12: Int = checkDistance(a: lines[0], b: lines[1])
+    let compare13: Int = checkDistance(a: lines[0], b: lines[2])
+    let compare23: Int = checkDistance(a: lines[1], b: lines[2])
+    
+    return compare12 + compare13 + compare23
+}
+
+func checkDistance(a: [Int], b: [Int]) -> Int {
+    let length = (a[1] < b[1] ? b[1] : a[1]) - (a[0] > b[0] ? b[0] : a[0])
+    let start = abs(a[0] - b[0])
+    let end = abs(a[1] - b[1])
+    let result = length - start - end
+    return result > 0 ? result : 0
+}
+
+//solution([[0, 1], [2, 5], [3, 9]])
+//solution([[-1, 1], [1, 3], [3, 9]])
+solution([[0, 5], [3, 9], [1, 10]])
 
 //: [Next](@next)
