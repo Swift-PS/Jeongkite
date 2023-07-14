10f5f488892be3178ff3b317024bf99cc3120770
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-14 21:02:48 +0900

    [PRO] 부족한 금액 계산하기 / Lv.1

--- Week3.playground/Pages/4_PRO_부족한 금액 계산하기.xcplaygroundpage/Contents.swift
+++ Week3.playground/Pages/4_PRO_부족한 금액 계산하기.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
+    var answer:Int64 = -1
+    var totalFee: Int = 0
+    
+    for fee in stride(from: price, through: price*count, by: price) {
+        totalFee += fee
+    }
+    
+    if totalFee - money > 0 {
+        answer = Int64(totalFee - money)
+    } else {
+        answer = 0
+    }
+    
+    return answer
+}
+
+solution(3, 20, 4)
 
 //: [Next](@next)
