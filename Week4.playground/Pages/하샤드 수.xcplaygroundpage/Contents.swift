981211efced0f3b63e75b657d168c67a04774c45
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-21 13:52:43 +0900

    [PRO] 하샤드 수 / Lv.1

--- Week4.playground/Pages/하샤드 수.xcplaygroundpage/Contents.swift
+++ Week4.playground/Pages/하샤드 수.xcplaygroundpage/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+//: [Previous](@previous)
 
-var greeting = "Hello, playground"
+import Foundation
+
+func solution(_ x:Int) -> Bool {
+    let sum: Int = getSum(x)
+    return x % sum == 0 ? true : false
+}
+
+func getSum(_ x: Int) -> Int {
+    var sum: Int = 0
+    var i: Int = x
+    
+    while i > 0 {
+        sum += i % 10
+        i /= 10
+    }
+    
+    return sum
+}
+
+solution(18)
+
+//: [Next](@next)
