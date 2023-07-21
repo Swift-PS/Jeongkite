e1fb2cd0b061c3d15f5d96826b50fb880a5562d0
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-21 22:34:16 +0900

    삼총사 풀이중

--- Week4.playground/Pages/삼총사.xcplaygroundpage/Contents.swift
+++ Week4.playground/Pages/삼총사.xcplaygroundpage/Contents.swift
@@ -2,6 +2,16 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ number:[Int]) -> Int {
+    for i in 0...number.count-2 {
+        for j in 1...number.count-1 {
+            for k in 2...number.count {
+                // 이거 count는 인덱스가 아니라 크기라 하나 빼줘야함
+            }
+        }
+    }
+    
+    return 0
+}
 
 //: [Next](@next)
