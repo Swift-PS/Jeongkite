eacdddfd5deeb7d068713d4b2ff8084aa01d1838
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-21 14:13:37 +0900

    [PRO] 예산 / Lv.1

--- Week4.playground/Pages/예산.xcplaygroundpage/Contents.swift
+++ Week4.playground/Pages/예산.xcplaygroundpage/Contents.swift
@@ -2,6 +2,19 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    var b: Int = budget
+    
+    for (i, a) in d.sorted().enumerated() {
+        b -= a
+        if b < 0 {
+            return i
+        }
+    }
+    return d.count
+}
+
+solution([1,3,2,5,4], 9)
+solution([2,2,3,3], 10)
 
 //: [Next](@next)
