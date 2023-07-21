4c161a414f0f32551e73e56a273cf6b6a866a749
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-21 13:17:22 +0900

    [PRO] 나머지가 1이 되는 수 찾기 / Lv.1

--- Week4.playground/Pages/나머지가 1이 되는 수 찾기.xcplaygroundpage/Contents.swift
+++ Week4.playground/Pages/나머지가 1이 되는 수 찾기.xcplaygroundpage/Contents.swift
@@ -2,6 +2,16 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ n:Int) -> Int {
+    for i in 2..<n-1 {
+        if n % i == 1 {
+            return i
+        }
+    }
+    return n-1
+}
+
+solution(10)
+solution(12)
 
 //: [Next](@next)
