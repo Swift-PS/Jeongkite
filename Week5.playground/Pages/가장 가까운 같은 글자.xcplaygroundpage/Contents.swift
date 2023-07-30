c1137adfa2dbc83d28b907bc467ddc3e902b6496
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-30 19:33:17 +0900

    [PRO] 가장 가까운 같은 글자

--- Week5.playground/Pages/가장 가까운 같은 글자.xcplaygroundpage/Contents.swift
+++ Week5.playground/Pages/가장 가까운 같은 글자.xcplaygroundpage/Contents.swift
@@ -3,7 +3,21 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    return []
+    var result: [Int] = []
+    for (index, char) in s.enumerated() {
+        var nearIndex: Int = -1
+        for (i, c) in Array(s)[0..<index].reversed().enumerated() {
+            if c == char {
+                nearIndex = i + 1
+                break
+            }
+        }
+        result.append(nearIndex)
+    }
+    
+    return result
 }
+
+solution("banana")
 
 //: [Next](@next)
