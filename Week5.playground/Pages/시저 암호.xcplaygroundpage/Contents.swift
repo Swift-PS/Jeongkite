898749aa255907774ac5a6e680c2032b3fa75384
Jeongyeon Yang <jeong6kite@gmail.com>
2023-07-30 18:39:38 +0900

    [PRO] 시저 암호 첫 풀이 / Lv.1

--- Week5.playground/Pages/시저 암호.xcplaygroundpage/Contents.swift
+++ Week5.playground/Pages/시저 암호.xcplaygroundpage/Contents.swift
@@ -4,8 +4,22 @@ import Foundation
 
 func solution(_ s:String, _ n:Int) -> String {
     var result: String = ""
+    for char in s {
+        var ascii = UnicodeScalar(String(char))!.value
+        if (UnicodeScalar("A").value <= ascii && ascii <= UnicodeScalar("Z").value
+            && (ascii + UInt32(n) > UnicodeScalar("Z").value)) {
+            ascii = UnicodeScalar("A").value + (ascii + UInt32(n) - UnicodeScalar("Z").value) - 1
+        } else if (UnicodeScalar("a").value <= ascii && ascii <= UnicodeScalar("z").value
+                   && (ascii + UInt32(n) > UnicodeScalar("z").value)) {
+            ascii = UnicodeScalar("a").value + (ascii + UInt32(n) - UnicodeScalar("z").value) - 1
+        } else if (String(char) != " ") {
+            ascii = ascii + UInt32(n)
+        }
+        result.append(String(UnicodeScalar(ascii)!))
+    }
     return result
 }
 
+solution("z", 1)
 
 //: [Next](@next)
