e7c2798e3aee3e7b60834c19c1ed0eef76dd6c80
Jeongyeon Yang <jeong6kite@gmail.com>
2023-08-09 21:32:04 +0900

    JadenCase 첫 풀이

--- Week7.playground/Pages/JadenCase 문자열 만들기.xcplaygroundpage/Contents.swift
+++ Week7.playground/Pages/JadenCase 문자열 만들기.xcplaygroundpage/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
+func solution(_ s:String) -> String {
+    return s.lowercased().split(separator: " ").map { $0[$0.startIndex].uppercased()+$0[$0.startIndex+1..<$0.endIndex] }.joined(separator: " ")
+}
+
+solution("3people unFollowed me")
 
