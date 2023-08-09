0fcecd337f12960755a871679b88dcf4587f5780
Jeongyeon Yang <jeong6kite@gmail.com>
2023-08-09 21:40:13 +0900

    두 번째 제이든

--- Week7.playground/Pages/JadenCase 문자열 만들기.xcplaygroundpage/Contents.swift
+++ Week7.playground/Pages/JadenCase 문자열 만들기.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
 import UIKit
 
 func solution(_ s:String) -> String {
-    return s.lowercased().split(separator: " ").map { $0[$0.startIndex].uppercased()+$0[$0.startIndex+1..<$0.endIndex] }.joined(separator: " ")
+    var isFirst: Bool = true
+    var result: String = ""
+    for c in s {
+        result.append(isFirst ? c.uppercased() : c.lowercased())
+        if c == " " {
+            isFirst = true
+        } else {
+            isFirst = false
+        }
+    }
+    
+//    return s.lowercased().split(separator: " ").map { $0[$0.startIndex].uppercased()+$0[$0.startIndex+1..<$0.endIndex] }.joined(separator: " ")
+    return result
 }
 
 solution("3people unFollowed me")
